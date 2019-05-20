bb681248e2d0aa1f996c949758e6265c50b609fd
Harlan Kellaway <harlan@prolificinteractive.com>
2019-05-20 14:25:57 -0400

    Installed networking pod

---
+++ PinballMap/Pods/Target Support Files/HottPotato/HottPotato-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HottPotato : NSObject
+@end
+@implementation PodsDummy_HottPotato
+@end
--- PinballMap/Pods/Target Support Files/Pods-PinballMap/Pods-PinballMap-frameworks.sh
+++ PinballMap/Pods/Target Support Files/Pods-PinballMap/Pods-PinballMap-frameworks.sh
@@ -161,10 +161,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/HottPotato/HottPotato.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TinyConstraints/TinyConstraints.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/HottPotato/HottPotato.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TinyConstraints/TinyConstraints.framework"
 fi
