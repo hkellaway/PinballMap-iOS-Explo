7ccc7704da55a7ecdf86236d81008aa35c6ff347
Harlan Kellaway <harlan@prolificinteractive.com>
2019-05-22 18:48:55 -0400

    MVVM visualize Machines state

--- PinballMap/PinballMap/MVVM/MVVMStateVisualizerViewController.swift
+++ PinballMap/PinballMap/MVVM/MVVMStateVisualizerViewController.swift
@@ -23,41 +23,50 @@ final class MVVMStateVisualizerViewController: StateVisualizerViewController {
     private func determineStateFromViewObjects() -> State {
         let rootTabBarController: RootTabBarController? = UIApplication.shared.delegate?.window??.rootViewController as? RootTabBarController
         
+        // Get selectedRegion and selectedLocation from shared Session:
+        
+        let selectedRegion: Region? = Session.shared.currentRegion
+        let selectedLocation: Location? = Session.shared.currenLocation
+        
         // Get Regions from RegionsViewModel:
         
         let regionsNavController: UINavigationController? = rootTabBarController?.viewControllers?[0] as? UINavigationController
         let regionsViewController: MVVMRegionsViewController? = regionsNavController?.viewControllers[0] as? MVVMRegionsViewController
         let regionsViewModel: RegionsViewModel? = regionsViewController?.viewModel
+        let regionList: APIResponseStatus<RegionList>? = regionsViewModel?.regions == nil
+            ? nil :
+            .loaded(success: RegionList(regions: regionsViewModel!.regions))
         
-        // Get Location from LocationsViewModel, if alive:
+        // Get Locations from LocationsViewModel, if alive:
         
         let locationsViewController: MVVMLocationsViewController? =
             (regionsNavController?.viewControllers.count ?? 1) >= 2
                 ? regionsNavController?.viewControllers[1] as? MVVMLocationsViewController
                 : nil
         let locationsViewModel: LocationsViewModel? = locationsViewController?.viewModel
-        
-        // Get selectedRegion and selectedLocation from shared Session:
-        
-        let selectedRegion: Region? = Session.shared.currentRegion
-        let selectedLocation: Location? = Session.shared.currenLocation
-        
-        // Convert to State obeject:
-        
-        let regionList: APIResponseStatus<RegionList>? = regionsViewModel?.regions == nil
-            ? nil :
-            .loaded(success: RegionList(regions: regionsViewModel!.regions))
         let locationList: APIResponseStatus<LocationList>? =
             locationsViewModel?.locations == nil
                 ? nil
                 : .loaded(success: LocationList(locations: locationsViewModel!.locations))
+        
+        // Get Machines from MachinesViewModel
+        
+        let machinesNavController: UINavigationController? = rootTabBarController?.viewControllers?[1] as? UINavigationController
+        let machinesViewController: MVVMMachinesViewController? = machinesNavController?.viewControllers[0] as? MVVMMachinesViewController
+        let machinesViewModel: MachinesViewModel? = machinesViewController?.viewModel
+        let machineList: APIResponseStatus<MachineList>? =
+            machinesViewModel?.machines == nil
+                ? nil
+                : .loaded(success: MachineList(machines: machinesViewModel!.machines))
+        
+        // Convert to State object:
         
         return State(selectedRegion: selectedRegion,
                      selectedLocation: selectedLocation,
                      selectedMachine: nil,
                      regionList: regionList,
                      locationList: locationList,
-                     machineList: nil)
+                     machineList: machineList)
     }
     
 }
