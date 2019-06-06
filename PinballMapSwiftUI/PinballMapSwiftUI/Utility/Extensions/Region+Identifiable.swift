741e31de5367d736cb22d0e22a3f5ef7b16f232a
Harlan Kellaway <harlan@prolificinteractive.com>
2019-06-06 15:43:32 -0400

    [SwiftUI] Marked Region as Identifiable

---
+++ PinballMapSwiftUI/PinballMapSwiftUI/Utility/Extensions/Region+Identifiable.swift
@@ -0,0 +1,12 @@
+//
+//  Region+Identifiable.swift
+//  PinballMapSwiftUI
+//
+//  Created by Harlan Kellaway on 6/6/19.
+//  Copyright © 2019 Harlan Kellaway. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Region: Identifiable { }
